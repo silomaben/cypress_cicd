77ccf49ffd05050a5f3c145da1b6bb500a369338
BenardSilomaMasikond <BenardSilomaMasikond@JITU-PC110VYA>
2024-03-01 17:33:46 +0300

    added jenkins file10

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    html:true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    log: true, 
+    quest: true
+  },
   e2e: {
     baseUrl: 'http://localhost:4200',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
